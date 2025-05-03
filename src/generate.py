3437eb20b77998a7feb7a339b86b63dee625d7a1
Prince Verma <sm24mtech11003@iith.ac.in>
2025-05-04 03:19:56 +0530

    Create generate.py

---
+++ src/generate.py
@@ -0,0 +1,10 @@
+import torch
+import pandas as pd
+
+def generate_synthetic_data(generator, data_shape, latent_dim, scaler, columns):
+    noise = torch.randn(data_shape[0], latent_dim)
+    synthetic_data = generator(noise).detach().numpy()
+    synthetic_data = scaler.inverse_transform(synthetic_data)
+    df_synthetic = pd.DataFrame(synthetic_data, columns=columns)
+    df_synthetic.to_csv("outputs/synthetic_data.csv", index=False)
+    return df_synthetic
