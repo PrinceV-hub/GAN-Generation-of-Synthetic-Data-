0a45c421704e07049736f4e75177f9f534956936
Prince Verma <sm24mtech11003@iith.ac.in>
2025-05-04 04:36:06 +0530

    Update generate.py

--- src/generate.py
+++ src/generate.py
@@ -6,5 +6,5 @@ def generate_synthetic_data(generator, data_shape, latent_dim, scaler, columns):
     synthetic_data = generator(noise).detach().numpy()
     synthetic_data = scaler.inverse_transform(synthetic_data)
     df_synthetic = pd.DataFrame(synthetic_data, columns=columns)
-    df_synthetic.to_csv("outputs/synthetic_data.csv", index=False)
+    df_synthetic.to_csv("synthetic_data.csv", index=False)
     return df_synthetic
